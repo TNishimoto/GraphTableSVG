c1a7b6e67b14271fd97d7c75f4e8b3f6de8bff68
MKLemma <a27b9c3@gmail.com>
2018-07-10 22:46:04 +0900

    tutorial

--- docs/graph/sample_graph_dash_style.js
+++ docs/graph/sample_graph_dash_style.js
@@ -3,7 +3,9 @@ window.onload = () => {
     const box = document.getElementById('svgbox');
     graph = new GraphTableSVG.Graph(box);
 
+    /* 点線の種類 */
     const dashStyles = ["msoLineDash", "msoLineDashDot", "msoLineDashDotDot" , "msoLineLongDash" , "msoLineLongDashDot" , "msoLineRoundDot", "msoLineSolid", "msoLineSquareDot"];
+    /* 辺上のテキストの配置方法 */
     const alignments = ["begin", "end", "center", "regularInterval"]
     for(let i=0;i<dashStyles.length;i++){
         const nodeLL = GraphTableSVG.Vertex.create(graph,{surfaceType: "rectangle", x : 50, y : (i+1) * 50});
@@ -11,7 +13,9 @@ window.onload = () => {
         const nodeC = GraphTableSVG.Vertex.create(graph,{surfaceType: "rectangle", x : 450, y : (i+1) * 50});
         const nodeR = GraphTableSVG.Vertex.create(graph,{surfaceType: "rectangle", x : 700, y : (i+1) * 50});
 
+        /* pathTextAlignmentで辺のテキストの配置方法を決定 */
         const edgeL = GraphTableSVG.Edge.create(graph, {beginVertex : nodeLL, endVertex : nodeL, text : alignments[i%alignments.length],pathTextAlignment : alignments[i%alignments.length] });
+        
         const edgeC = GraphTableSVG.Edge.create(graph, {beginVertex : nodeL, endVertex : nodeC, text : dashStyles[i]});
         const edgeR = GraphTableSVG.Edge.create(graph, {beginVertex : nodeC, endVertex : nodeR});
 
@@ -19,6 +23,7 @@ window.onload = () => {
         edgeC.svgPath.style.strokeWidth="3px"; 
         edgeR.svgPath.style.strokeWidth="6px";
 
+        /* 点線の設定　*/
         GraphTableSVG.msoDashStyle.setStyle(edgeL.svgPath, dashStyles[i]);
         GraphTableSVG.msoDashStyle.setStyle(edgeC.svgPath, dashStyles[i]);
         GraphTableSVG.msoDashStyle.setStyle(edgeR.svgPath, dashStyles[i]);
--- docs/graph/sample_graph_tree.js
+++ docs/graph/sample_graph_tree.js
@@ -3,6 +3,7 @@ let graph = null;
 window.onload = () => {
     const box = document.getElementById('svgbox');
     graph = new GraphTableSVG.Graph(box);
+    /* ノードをグラフの根にしたい場合はisRootにTrueを入れるか、graph.roots配列にノードを追加すること */
     const node1 = GraphTableSVG.Vertex.create(graph, {text:"1", isRoot : true});
     const node2 = GraphTableSVG.Vertex.create(graph, {text:"2"});
     const node3 = GraphTableSVG.Vertex.create(graph, {text:"3"});
@@ -16,6 +17,7 @@ window.onload = () => {
     const node11 = GraphTableSVG.Vertex.create(graph, {text:"11"});
     const node12 = GraphTableSVG.Vertex.create(graph, {text:"12"});
 
+    /* 第一引数のVertexに第二引数のVertexを子として追加する。二点間の辺は自動で作成される */
     graph.appendChild(node1, node2);
     graph.appendChild(node1, node3);
     graph.appendChild(node3, node4);
@@ -28,9 +30,11 @@ window.onload = () => {
     graph.appendChild(node8, node11);
     graph.appendChild(node8, node12);
 
+    /* 隣り合う頂点間の水平方向の間隔を設定 */
     graph.vertexXInterval = 70;
+    /* 隣り合う頂点間の垂直方向の間隔を設定 */
     graph.vertexYInterval = 70;
-
+    /* 木の各頂点の配置方法を選択 */
     graph.relocateFunction = GraphTableSVG.TreeArrangement.alignVerticeByLeave;
 
 
