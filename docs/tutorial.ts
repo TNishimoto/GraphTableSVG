55afb68b8ee2a549a225cd29a8f68330f560afbf
MKLemma <a27b9c3@gmail.com>
2018-07-14 01:05:54 +0900

    tutorial

--- docs/tutorial.ts
+++ docs/tutorial.ts
@@ -1,5 +1,5 @@
 
-import { SET, Macroup, MacroupLib,HTMLLib,libxmljs } from 'macroup';
+import { SET, Macroup, MacroupLib, HTMLLib, libxmljs } from 'macroup';
 import path = require('path');
 
 const pack = new Macroup.MacroPackage("tutorial");
@@ -23,27 +23,27 @@ function addNextSiblingCode(e : libxmljs.Element, codepaths : string[], dir : st
     }
 }
 */
-function createLoadCode(e : libxmljs.Element, dir : string) : libxmljs.Element {
+function createLoadCode(e: libxmljs.Element, dir: string): libxmljs.Element {
     const filePath = e.attr("path").value();
     const ext = path.extname(filePath);
-    if(ext == ".png"){
+    if (ext == ".png") {
         const img = new libxmljs.Element(e.doc(), "img", "");
-        img.attr({src:filePath, "style":"max-width:100%" });
+        img.attr({ src: filePath, "style": "max-width:100%" });
         //img.attr({src:v });        
         return img;
-    }else{
+    } else {
         return HTMLLib.createReferenceCodeTag(filePath, dir, e.doc());
     }
 }
 
 
-function moveChildren(oldNode : libxmljs.Element, newNode : libxmljs.Element){
-    oldNode.childNodes().forEach((v)=>newNode.addChild(v));
+function moveChildren(oldNode: libxmljs.Element, newNode: libxmljs.Element) {
+    oldNode.childNodes().forEach((v) => newNode.addChild(v));
 }
-function clearChildren(e : libxmljs.Element){
-    e.childNodes().forEach((v)=>v.remove());
+function clearChildren(e: libxmljs.Element) {
+    e.childNodes().forEach((v) => v.remove());
 }
-function parseXML(str : string) : libxmljs.Element {
+function parseXML(str: string): libxmljs.Element {
     const doc = libxmljs.parseXmlString(str);
     return doc.root();
 }
@@ -54,11 +54,11 @@ pack.midMacros.elements["p"] = (e : libxmljs.Element, info : Macroup.Setting) =>
     //(<any>e).text("　" + text);
 }
 */
-function tempora(lines : libxmljs.Element[], e : libxmljs.Element) : libxmljs.Element {
+function tempora(lines: libxmljs.Element[], e: libxmljs.Element): libxmljs.Element {
     //MacroupLib.insertFirst(v, "　");
-    const sectionNode = new libxmljs.Element(e.doc(), "section", undefined); 
-    sectionNode.attr({"ignore-format" : "true", class : "sample-commentary"});
-    lines.forEach((w)=>{
+    const sectionNode = new libxmljs.Element(e.doc(), "section", undefined);
+    sectionNode.attr({ "ignore-format": "true", class: "sample-commentary" });
+    lines.forEach((w) => {
         //console.log(w.text());
 
         sectionNode.addChild(w);
@@ -72,101 +72,114 @@ function tempora(lines : libxmljs.Element[], e : libxmljs.Element) : libxmljs.El
     return sectionNode;
 
 }
-const hrefDic :{ [key: string]: string; }={
-    LogicTree:"./typedoc/classes/graphtablesvg.logictree.html",
-    LogicCell:"./typedoc/classes/graphtablesvg.logiccell.html",
-    LogicTable:"./typedoc/classes/graphtablesvg.logictable.html",
-    "TreeArrangement.alignVerticeByChildren":"./typedoc/modules/graphtablesvg.treearrangement.html#alignverticebychildren",
-    "TreeArrangement.alignVerticeByLeave":"./typedoc/modules/graphtablesvg.treearrangement.html#alignverticebyleave",
-    "graph.vertexXInterval":"./typedoc/classes/graphtablesvg.graph.html#vertexxinterval",
-    "graph.vertexYInterval":"./typedoc/classes/graphtablesvg.graph.html#vertexyinterval"
-}
-function setXMLText(e : libxmljs.Element, text : string){
+const hrefDic: { [key: string]: string; } = {}
+function setXMLText(e: libxmljs.Element, text: string) {
     const result = libxmljs.parseXml(text);
     clearChildren(e);
     e.addChild(result.root());
     //result.root().childNodes().forEach((v)=>e.addChild(v));
 }
-function replaceXMLText(e : libxmljs.Element, text : string){
+function replaceXMLText(e: libxmljs.Element, text: string) {
     const result = libxmljs.parseXml(text);
     clearChildren(e);
     e.addPrevSibling(result.root());
     e.remove();
     //e.addChild(result.root());
 }
-pack.midMacros.elements["ahref"] = (e : libxmljs.Element, info : Macroup.Setting) => {
-    const name = e.text();
-    if(name in hrefDic){
-        replaceXMLText(e, `<a href="${hrefDic[name]}" target="_blank">${name}</a>`);
-        
-    }else{
-        replaceXMLText(e, `<p style="color:red;font-size:24pt">${name} : No Exist Link!</p>`);
+pack.midMacros.elements["ahref"] = (e: libxmljs.Element, info: Macroup.Setting) => {
+    const name1 = e.attr("key");
+    if (name1 == null) {
+        const name = e.text();
+        if (name in hrefDic) {
+            replaceXMLText(e, `<a href="${hrefDic[name]}" target="_blank">${name}</a>`);
 
-        //(<any>e).text("No Dic!");
+        } else {
+            replaceXMLText(e, `<p style="color:red;font-size:24pt">${name} : No Exist Link!</p>`);
+
+            //(<any>e).text("No Dic!");
+        }
+    } else {
+        const url = e.attr("url");
+        hrefDic[name1.value()] = url.value();
     }
 }
-pack.midMacros.elements["xarticle"] = (e : libxmljs.Element, info : Macroup.Setting) =>{
-    e.attr({after : "article"});
-    e.attr({class:"sample-article"})
+pack.midMacros.elements["rhref"] = (e: libxmljs.Element, info: Macroup.Setting) =>{
+    const isMethod = e.attr("method") != null;
+    const text = e.text();
+    if(isMethod){
+        const texts = text.split(".");
+        const methodName = texts[texts.length-1].toLowerCase();
+        let className = "";
+        if(texts.length == 2){
+            className = texts[0].toLowerCase();
+        }
+        replaceXMLText(e, `<a href="./typedoc/classes/graphtablesvg.${className}.html#${methodName}" target="_blank">${text}</a>`);
+    }else{
+        replaceXMLText(e, `<a href="./typedoc/classes/graphtablesvg.${text}.html" target="_blank">${text}</a>`);
+    }
+}
+pack.midMacros.elements["xarticle"] = (e: libxmljs.Element, info: Macroup.Setting) => {
+    e.attr({ after: "article" });
+    e.attr({ class: "sample-article" })
 
     const dir = path.dirname(info.inputPath);
     const nodes = e.childNodes();
-    const newNodes : libxmljs.Element[] = [];
+    const newNodes: libxmljs.Element[] = [];
     const title = new libxmljs.Element(e.doc(), "h2", e.attr("title")!.value());
     newNodes.push(title);
 
-    const tmp : libxmljs.Element[] = [];
-    nodes.forEach((v)=>{
-        if(v.name() == "comment"){
-            if(tmp.length > 0) newNodes.push(tempora(tmp, e));
+    const tmp: libxmljs.Element[] = [];
+    nodes.forEach((v) => {
+        if (v.name() == "comment") {
+            if (tmp.length > 0) newNodes.push(tempora(tmp, e));
             MacroupLib.insertFirst(v, "　");
-            const sectionNode = new libxmljs.Element(e.doc(), "section", undefined); 
-            sectionNode.attr({"ignore-format" : "true", class : "sample-commentary"});
+            const sectionNode = new libxmljs.Element(e.doc(), "section", undefined);
+            sectionNode.attr({ "ignore-format": "true", class: "sample-commentary" });
             moveChildren(v, sectionNode);
             newNodes.push(sectionNode);
-        }else if(v.name() == "load"){
-            if(tmp.length > 0) newNodes.push(tempora(tmp, e));
+        } else if (v.name() == "load") {
+            if (tmp.length > 0) newNodes.push(tempora(tmp, e));
             newNodes.push(createLoadCode(v, dir));
         }
-        else if(v.name() == "dbr"){
+        else if (v.name() == "dbr") {
             //if(title.text() == "はじめに")console.log(v.text());
 
-            if(tmp.length > 0) newNodes.push(tempora(tmp, e));
+            if (tmp.length > 0) newNodes.push(tempora(tmp, e));
         }
-        else{
-            if(v.text() != "\n"){
+        else {
+            if (v.text() != "\n") {
                 tmp.push(v);
             }
         }
     })
-    if(tmp.length > 0) newNodes.push(tempora(tmp, e));
+    if (tmp.length > 0) newNodes.push(tempora(tmp, e));
 
     clearChildren(e);
-    newNodes.forEach((v)=>{    
+    newNodes.forEach((v) => {
         //if(title.text() == "はじめに")console.log(v.text());
         e.addChild(v)
     });
     //if(title.text() == "はじめに")console.log(e.toString());
 
 }
-pack.midMacros.elements["table_of_contents"]= (e : libxmljs.Element, info : Macroup.Setting) =>{
-    e.attr({after : "div"});
+pack.midMacros.elements["table_of_contents"] = (e: libxmljs.Element, info: Macroup.Setting) => {
+    e.attr({ after: "div" });
 
     const title = new libxmljs.Element(e.doc(), "h2", "目次");
     e.addChild(title);
-    
+
     const xarticleNodes = e.doc().find("//xarticle");
 
     const listNode = new libxmljs.Element(e.doc(), "ol", undefined);
     let idCounter = 0;
-    xarticleNodes.forEach((v)=> {
+    xarticleNodes.forEach((v) => {
         const titleName = v.attr("title").value();
         const idValue = `xarticle-${idCounter++}`;
-        v.attr({id : idValue });
+        v.attr({ id: idValue });
         listNode.addChild(libxmljs.parseXmlString(`<li><a href="#${idValue}">${titleName}</a></li>`).root());
     });
     e.addChild(listNode);
-    e.attr({class:"sample-table-of-contents"});
+    e.attr({ class: "sample-table-of-contents" });
 }
 
 SET.addPackage(pack);
