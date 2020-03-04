b72d8bde0ef40c4edf4a9338f455defd6f500b6f
TNishimoto <a32b16c4@gmail.com>
2020-03-05 01:49:00 +0900

    npm

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
 
 # GraphTableSVG
-
-## Introduction
-表とグラフ（頂点と枝のあれ）をSVGで描画するためのtypescriptライブラリ
-
+GraphTableSVG is a library outputting graphs and tables on svg.
 [reference](https://TNishimoto.github.io/GraphTableSVG/typedoc/index.html "reference")
-いろいろと未完成
-
-## What can GraphtableSVG do?
-1. SVGで表の描画
-2. SVGでグラフの描画
-3. 作成した表やグラフをpowerpointのマクロに変換
-
-3はおまけ
-
-## Tutorial
 [tutorial](https://TNishimoto.github.io/GraphTableSVG/tutorial.html "tutorial")
 
-## License？
+## License
 MIT License
 
-## その他
-Firefox, Chrome, Edgeで動作を確認中。
-PNG画像の生成にはhttps://st40.xyz/one-run/article/133/のコードを使用しました。
+## Environment
+Firefox, Chrome, Edge
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
     mode: 'development',
-    entry: './src/index.ts', //ファイルをまとめる際のエントリーポイント
+    entry: './src/index.ts',
     output: {
         library: "GraphTableSVG",
         path: __dirname,
-        filename: 'docs/scripts/graph_table_svg.js' //まとめた結果出力されるファイル名
+        filename: 'docs/scripts/graph_table_svg.js'
     },
     resolve: {
-        extensions: ['.ts', '.js'] //拡張子がtsだったらTypescirptでコンパイルする
+        extensions: ['.ts', '.js']
     },
     module: {
         rules: [{
             test: /\.ts$/,
-            loader: 'ts-loader' //ts-loader使うよ
+            loader: 'ts-loader'
         }]
     }
 }
